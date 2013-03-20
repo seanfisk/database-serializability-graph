c9a25c0031b3f5d3e1acc303e31f38a7296392e4
Sean Fisk <sean@seanfisk.com>
2013-03-20 19:54:00 -0400

    Correctly detect all transaction numbers.

--- generate_graph.py
+++ generate_graph.py
@@ -34,23 +34,25 @@ args = arg_parser.parse_args()
 line_re = re.compile(r'([rw])(\d+)\((\w+)\)$')
 
 operations = []
+transactions = set()
 
 for line_no, line in enumerate(args.input_file):
     line_match = line_re.match(line.rstrip())
     if line_match is None:
         raise SystemExit(
             'Incorrect input file syntax on line {0}.'.format(line_no))
-    operations.append(Operation(line_match.group(1) == 'w',
-                                int(line_match.group(2)),
-                                line_match.group(3)))
+    is_write = line_match.group(1) == 'w'
+    transaction_number = int(line_match.group(2))
+    data_item = line_match.group(3)
+    operations.append(Operation(is_write, transaction_number, data_item))
+    transactions.add(transaction_number)
+
 args.input_file.close()
 
-transactions = set()
 conflicts = set()
 
 for i in xrange(len(operations) - 1):
     op1 = operations[i]
-    transactions.add(op1.transaction_number)
     for j in xrange(i + 1, len(operations)):
         op2 = operations[j]
         if ((op1.is_write or op2.is_write) and
