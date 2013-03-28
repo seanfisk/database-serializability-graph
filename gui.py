a4f1638df76e2a181ebb5c6796c3fb1403bee2ca
Sean Fisk <sean@seanfisk.com>
2013-03-27 21:16:14 -0400

    GUI displays text area and SVG file.

--- gui.py
+++ gui.py
@@ -4,12 +4,27 @@ import sys
 
 from PySide import QtGui, QtSvg
 
+class MainWindow(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+
+        self.central_widget = QtGui.QWidget()
+        self.central_layout = QtGui.QHBoxLayout(self.central_widget)
+
+        self.input_area = QtGui.QTextEdit()
+        self.central_layout.addWidget(self.input_area)
+
+        self.output_area = QtSvg.QSvgWidget('out.svg')
+        self.central_layout.addWidget(self.output_area)
+
+        self.setCentralWidget(self.central_widget)
+
 def main(argv):
     app = QtGui.QApplication(argv)
 
-    w = QtSvg.QSvgWidget('out.svg')
-    w.show()
-    w.raise_()
+    win = MainWindow()
+    win.show()
+    win.raise_()
 
     app.exec_()
     return 0
