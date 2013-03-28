a5a43af8eb9eced7daed40aff1e02ba208dcebcf
Sean Fisk <sean@seanfisk.com>
2013-03-28 00:32:45 -0400

    Make a slick GUI.

--- serial_graph/gui.py
+++ serial_graph/gui.py
@@ -3,8 +3,11 @@
 """
 
 import sys
+from cStringIO import StringIO
 
-from PySide import QtGui, QtSvg
+from PySide import QtCore, QtGui, QtSvg
+
+from serial_graph.graph import generate_serializability_graph
 
 
 class MainWindow(QtGui.QMainWindow):
@@ -14,13 +17,28 @@ class MainWindow(QtGui.QMainWindow):
         self.central_widget = QtGui.QWidget()
         self.central_layout = QtGui.QHBoxLayout(self.central_widget)
 
-        self.input_area = QtGui.QTextEdit()
-        self.central_layout.addWidget(self.input_area)
+        self.form_layout = QtGui.QVBoxLayout()
+        self.input_area = QtGui.QPlainTextEdit()
+        self.form_layout.addWidget(self.input_area)
+        self.submit_button = QtGui.QPushButton('Submit')
+        self.submit_button.clicked.connect(self._submit_button_clicked)
+        self.form_layout.addWidget(self.submit_button)
+        self.central_layout.addLayout(self.form_layout)
 
-        self.output_area = QtSvg.QSvgWidget('out.svg')
+        self.output_area = QtSvg.QSvgWidget()
         self.central_layout.addWidget(self.output_area)
 
         self.setCentralWidget(self.central_widget)
+
+    def _submit_button_clicked(self):
+        schedule_file = StringIO(self.input_area.toPlainText())
+        graph = generate_serializability_graph(schedule_file)
+        schedule_file.close()
+
+        output_svg_file = StringIO()
+        graph.draw(output_svg_file, format='svg')
+        self.output_area.load(QtCore.QByteArray(output_svg_file.getvalue()))
+        output_svg_file.close()
 
 
 def main(argv):
