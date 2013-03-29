a20c56e33d2fa0db0dc7a30d5823caf8e3d8e87a
Sean Fisk <sean@seanfisk.com>
2013-03-28 21:32:43 -0400

    Get a scaled pixmap working.

--- serial_graph/gui.py
+++ serial_graph/gui.py
@@ -15,6 +15,28 @@ class SvgWidget(QtSvg.QSvgWidget):
         return super(SvgWidget, self).sizeHint() * 3
 
 
+class ResizeLabel(QtGui.QLabel):
+    def resizeEvent(self, resize_event):
+        self.widget_size = resize_event.size()
+        super(ResizeLabel, self).resizeEvent(resize_event)
+
+    def paintEvent(self, paint_event):
+        painter = QtGui.QPainter(self)
+
+        if self.pixmap:
+            center_point = QtCore.QPoint(0, 0)
+            # Scale new image.
+            scaled_pixmap = self.pixmap().scaled(
+                self.widget_size, QtCore.Qt.KeepAspectRatio)
+            # Calculate image center position into screen.
+            center_point.setX(
+                (self.widget_size.width() - scaled_pixmap.width()) / 2)
+            center_point.setY(
+                (self.widget_size.height() - scaled_pixmap.height()) / 2)
+            # Draw image.
+            painter.drawPixmap(center_point, scaled_pixmap)
+
+
 class MainWindow(QtGui.QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
@@ -30,17 +52,23 @@ class MainWindow(QtGui.QMainWindow):
         self.form_layout.addWidget(self.submit_button)
         self.central_layout.addLayout(self.form_layout, 1)
 
-        self.output_area = SvgWidget()
-        size_policy = QtGui.QSizePolicy(QtGui.QSizePolicy.MinimumExpanding,
-                                        QtGui.QSizePolicy.MinimumExpanding)
-        self.output_area.setSizePolicy(size_policy)
+        self.output_area = ResizeLabel()
+        self.output_area.setPixmap(
+            QtGui.QPixmap.fromImage(QtGui.QImage('out.png')))
+        # self.output_area.setPixmap(pixmap.scaled(self.output_area.size(),
+        #                                          QtCore.Qt.KeepAspectRatio))
+
+        # self.output_area = SvgWidget()
+        # size_policy = QtGui.QSizePolicy(QtGui.QSizePolicy.MinimumExpanding,
+        #                                 QtGui.QSizePolicy.MinimumExpanding)
+        # self.output_area.setSizePolicy(size_policy)
         self.central_layout.addWidget(
-            self.output_area, 1, QtCore.Qt.AlignCenter)
+            self.output_area, 1)
 
         self.setCentralWidget(self.central_widget)
 
-        self._load_default_data()
-        self._submit_button_clicked()
+        # self._load_default_data()
+        # self._submit_button_clicked()
 
     def _load_default_data(self):
         self.input_area.setPlainText('''r1(X)
